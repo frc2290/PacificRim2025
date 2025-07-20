f8d4b67da0d4de62fa90a91a495cb04167dbf934
nacamm <nate@natecamm.dev>
2025-07-19 23:04:46 -0500

    Forgot to enable interpolation for auto

--- src/main/java/frc/robot/subsystems/StateSubsystem.java
+++ src/main/java/frc/robot/subsystems/StateSubsystem.java
@@ -481,8 +481,9 @@ public class StateSubsystem extends SubsystemBase {
         }
 
         /** Diff Arm Interpolation */
-        if (atInterpolateScoreState() && diff.hasLaserCanDistance() && !isAuto() && manipulator.hasCoral()) {
-            if (currentState == PositionState.L4Position) {
+        //if (atInterpolateScoreState() && diff.hasLaserCanDistance() && !isAuto() && manipulator.hasCoral()) {
+        if (atInterpolateScoreState() && diff.hasLaserCanDistance() && manipulator.hasCoral()) {
+                if (currentState == PositionState.L4Position) {
                 diff.setExtensionSetpoint(diff.l4ExtensionInterpolate());
                 diff.setRotationSetpoint(diff.l4RotationInterpolate());
             } else {
