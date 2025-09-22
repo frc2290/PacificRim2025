a82fc46ddbe9bb7ca81163ea818592acc2e5983d
Jake825 <jellenberger92@gmail.com>
2025-09-21 20:49:20 -0500

    Copied license header thing from 6328 spotless setup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,9 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright (c) 2025 FRC 2290
+// http://https://github.com/frc2290
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
 
 package frc.robot;
 
--- src/main/java/frc/robot/subsystems/ManipulatorStateMachine.java
+++ src/main/java/frc/robot/subsystems/ManipulatorStateMachine.java
@@ -1,3 +1,10 @@
+// Copyright (c) 2025 FRC 2290
+// http://https://github.com/frc2290
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.XboxController;
