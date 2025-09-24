bbf45f0c29576f6f70c64a51f0da5ddbaf2d3c54
Jake825 <jellenberger92@gmail.com>
2025-09-23 20:45:47 -0500

    make score algae more like score coral

--- src/main/java/frc/robot/commands/EndEffector/ScoreAlgae.java
+++ src/main/java/frc/robot/commands/EndEffector/ScoreAlgae.java
@@ -24,14 +24,11 @@ import frc.robot.subsystems.ManipulatorSubsystem;
 /** Spins the manipulator roller to dump algae into a processor or barge. */
 public class ScoreAlgae extends Command {
 
-  private static final double kEjectCurrentThresholdAmps = 30.0;
-  private static final double kEjectDelaySeconds = 1.0;
   private static final double kEjectDurationSeconds = 0.5;
 
   private final ManipulatorStateMachine manipulatorSM;
   private final ManipulatorSubsystem manipulator;
   private final Timer ejectTimer = new Timer();
-  private final Timer delayTimer = new Timer();
   private final double ejectPower;
 
   private boolean spinning = false;
@@ -64,43 +61,24 @@ public class ScoreAlgae extends Command {
   public void initialize() {
     ejectTimer.stop();
     ejectTimer.reset();
-    delayTimer.stop();
-    delayTimer.reset();
     spinning = false;
   }
 
   @Override
   public void execute() {
-    if (!spinning && manipulatorSM.atGoalState() && manipulatorSM.scoreNow()) {
+    boolean readyToSpin = manipulatorSM.atGoalState() && manipulatorSM.scoreNow();
+
+    if (readyToSpin && !spinning) {
       spinning = true;
-      delayTimer.stop();
-      delayTimer.reset();
-      ejectTimer.stop();
-      ejectTimer.reset();
+      ejectTimer.restart();
     }
 
-    if (spinning) {
-      manipulator.intake(ejectPower);
-
-      if (Math.abs(manipulator.getOutputCurrent()) > kEjectCurrentThresholdAmps) {
-        if (!delayTimer.isRunning()) {
-          delayTimer.restart();
-        } else if (!ejectTimer.isRunning() && delayTimer.hasElapsed(kEjectDelaySeconds)) {
-          delayTimer.stop();
-          ejectTimer.restart();
-        }
-      }
-    } else {
-      manipulator.intake(0);
-    }
+    manipulator.intake(spinning ? ejectPower : 0);
   }
 
   @Override
   public void end(boolean interrupted) {
     ejectTimer.stop();
-    ejectTimer.reset();
-    delayTimer.stop();
-    delayTimer.reset();
     manipulator.intake(0);
     if (!interrupted) {
       manipulator.setCoral(false);
@@ -112,6 +90,6 @@ public class ScoreAlgae extends Command {
 
   @Override
   public boolean isFinished() {
-    return ejectTimer.hasElapsed(kEjectDurationSeconds);
+    return spinning && ejectTimer.hasElapsed(kEjectDurationSeconds);
   }
 }
